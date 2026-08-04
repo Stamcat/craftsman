cc818fd6df1c598c266339ddbf17de9cc5cb18e5
Devin Marra <devinm@herbalife.com>
2026-08-04 12:54:42 -0700

    fix type error

--- src/stories/molecules/Modal.stories.tsx
+++ src/stories/molecules/Modal.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-import { Modal, type ModalType } from "../../components/Modal";
+import { Modal, type ModalType } from "../../components/Modal/Modal";
 import { Button } from "../../components/Button";
 
 
